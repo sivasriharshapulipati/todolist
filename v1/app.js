c8ad70d9f37cc2c9642d25a68c6554f3cf0b60c5
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-06-06 01:33:28 +0530

    work list added

--- v1/app.js
+++ v1/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -19,17 +20,23 @@ app.get("/",function(req,res){
   };
 
   let day = today.toLocaleDateString("en-US",options)
-  res.render("lists",{kindOfDay:day , newListItems:items});
+  res.render("list",{listTitle:day , newListItems:items});
 });
 
 app.post("/",function(req,res){
   let item = req.body.newitem;
-  items.push(item);
-
-  res.redirect("/");
-
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
 
 app.listen(3000,function() {
     console.log("server is up and running on port 3000");
