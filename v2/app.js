cf07718ccdba5ea1a47eb3124f39fe18dc101629
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-06-27 19:45:20 +0530

    delete implemented when checked

--- v2/app.js
+++ v2/app.js
@@ -1,90 +1,3 @@
-//jshint esversion:6
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static("public"));
-
-// // mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
-// mongoose.connect("mongodb://0.0.0.0:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-
-// const itemsSchema = {
-//   name:String
-
-// };
-
-// const Item = mongoose.model("Item",itemsSchema);
-
-// const item1 = new Item({
-//   name:"welcome to ur todo list"
-// });
-
-// const item2 = new Item({
-//   name:"hello"
-// });
-
-// const item3 = new Item({
-//   name:"wonder"
-// });
-
-// const defaultItems = [item1,item2,item3];
-
-// Item.insertMany(defaultItems)
-//       .then(result => {
-//         console.log('Default items inserted:');
-//       })
-//       .catch(error => {
-//         console.error('Error inserting default items:', error);
-//       });
-//   })
-//   .catch(error => {
-//     console.error('Error connecting to MongoDB:', error);
-//   });
-
-//   app.get("/", function(req, res) {
-//     Item.find({}, function(err, foundItems) {
-//       res.render("list", { listTitle: "Today", newListItems: foundItems });
-//     });
-//   });
-
-
-
-// app.post("/", function(req, res){
-
-//   const item = req.body.newItem;
-
-//   if (req.body.list === "Work") {
-//     workItems.push(item);
-//     res.redirect("/work");
-//   } else {
-//     items.push(item);
-//     res.redirect("/");
-//   }
-// });
-
-// app.get("/work", function(req,res){
-//   res.render("list", {listTitle: "Work List", newListItems: workItems});
-// });
-
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
-
-// app.listen(3000, function() {
-//   console.log("Server started on port 3000");
-// });
-
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -124,18 +37,22 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems)
-  .then(result => {
-    console.log('Default items inserted:');
-  })
-  .catch(error => {
-    console.error('Error inserting default items:', error);
-  });
-
 app.get("/", async function(req, res) {
   try {
     const foundItems = await Item.find({});
-    res.render("list", { listTitle: "Today", newListItems: foundItems });
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems)
+        .then(result => {
+          console.log('Default items inserted');
+        })
+        .catch(error => {
+          console.error('Error inserting default items:', error);
+        });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
   } catch (error) {
     console.error('Error finding items:', error);
     res.status(500).send('Internal Server Error');
@@ -143,16 +60,30 @@ app.get("/", async function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+  item.save();
+
+  res.redirect("/")
+
 });
+
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findOneAndRemove({ _id: checkedItemId })
+    .then(() => {
+      console.log("Successfully deleted checked item");
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.error("Error deleting checked item:", error);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
 
 app.get("/work", function(req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
